479a06150fc5aaac43e5f3d7454c8296e0b1adcb
wchamber01 <williamchambers@gmail.com>
2019-12-09 17:58:01 -0500

    refactored app.js to include ProductContext

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import data from "./data";
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
+import ProductContext from "./contexts/ProductContext";
 
 function App() {
   const [products] = useState(data);
@@ -20,16 +21,12 @@ function App() {
 
   return (
     <div className="App">
-      <Navigation cart={cart} />
-
-      {/* Routes */}
-      <Route
-        exact
-        path="/"
-        render={() => <Products products={products} addItem={addItem} />}
-      />
-
-      <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+      <ProductContext.Provider value={{ products, addItem }}>
+        <Navigation cart={cart} />
+        {/* Routes */}
+        <Route exact path="/" component={Products} />
+        <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+      </ProductContext.Provider>
     </div>
   );
 }
